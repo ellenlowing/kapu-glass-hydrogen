307bf43c15ff1f6e7f0bd7dcca60888964216fd4
Ellen Lo <lowing@bu.edu>
2023-07-13 13:22:40 -0700

    set loading screen for homepage

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -2,9 +2,32 @@ import Sketch from "./sketch/Sketch";
 import logo from '../../public/logo.png';
 import {Link, useLoaderData} from '@remix-run/react';
 import {isMobile} from 'react-device-detect';
+import {useEffect, useState} from 'react';
 
 export function Layout({children, title}) {
     const {cart} = useLoaderData();
+    const [loading, setLoading] = useState(true);
+    const [isHome, setIsHome] = useState(false);
+    
+    useEffect(() => {
+      if(window.location.pathname === '/') {
+        setIsHome(true);
+        // let opacity = 1;
+        // const fadeInterval = setInterval(() => {
+        //   loadingText.style.opacity = opacity;
+        //   opacity -= 0.05;
+        //   console.log(opacity,loadingText.style.opacity);
+        // }, 50);
+        setTimeout(() => {
+          // clearInterval(fadeInterval);
+
+          setLoading(false);
+        }, 2000);
+
+      } else {
+        setLoading(false);
+      }
+    }, [])
 
     return (
       <div className="flex flex-col min-h-screen antialiased">
@@ -12,11 +35,12 @@ export function Layout({children, title}) {
           role="banner"
           className={`absolute items-center h-20 z-[60] top-0 justify-between leading-none p-1 antialiased transition`}
         >
-          <Link className="h-full" to="/">
+          
+          <Link className={`h-full ${isHome ? 'transition ease-in-out duration-[1000ms] delay-500' : ''}  ${!loading ? 'opacity-100' : 'opacity-0'}`} to="/">
             <img id="logo" className="h-full" src={logo}></img>
           </Link>
-
-          <nav id="nav" className={`fixed z-[70] h-[40px]`}>
+          
+          <nav id="nav" className={`fixed z-[70] h-[40px] ${isHome ? 'hidden' : ''}`}>
             <Link id="nav-link-0" to="/collections/vessels" className=" cursor-pointer nav-link">vessels</Link>
             <Link id="nav-link-1" to="/collections/accessories" className=" cursor-pointer nav-link">accessories</Link>
             <Link id="nav-link-2" to="/collections/magazine" className=" cursor-pointer nav-link">magazine</Link>
@@ -27,7 +51,7 @@ export function Layout({children, title}) {
             <a id="menu-switch" className=" cursor-pointer nav-link">menu</a>
           </nav>
 
-          <canvas id="ladder-menu" className="fixed w-[160px] h-[320px] top-[0] right-[5vw] translate-y-[-280px] z-[60] mix-blend-multiply"></canvas>
+          <canvas id="ladder-menu" className={`fixed w-[160px] h-[320px] top-[0] right-[5vw] translate-y-[-280px] z-[60] mix-blend-multiply ${isHome ? 'hidden' : 'visible'}`}></canvas>
         </header>
 
         <main
@@ -35,9 +59,13 @@ export function Layout({children, title}) {
           id="mainContent"
           className={`flex-grow ${isMobile ? 'select-none' : ''}`}
         >
-          {children}
-
-          <Sketch/>
+          <div id="loading-text" className={`w-full select-none fa text-uppercase text-3xl absolute text-center top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ${isHome ? 'visible' : 'hidden'} transition ease-in-out duration-[1000ms] ${loading ? 'opacity-100' : 'opacity-0'}`}>
+            Right here is a good place to start
+          </div> 
+          <div className={`${isHome ? 'transition ease-in-out duration-[1000ms] delay-500' : ''} ${!loading ? 'opacity-100' : 'opacity-0'}`}>
+            {children}
+            <Sketch/>
+          </div>
         </main>
       </div>
     );
